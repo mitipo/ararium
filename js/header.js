b872fded0f293145a316acb39d747ddbf293e4ce
HamB20001013 <khb1535@gmail.com>
2023-08-23 15:17:49 +0900

    feat : header 반응형 작업중
    header 반응형 작업 중

--- js/header.js
+++ js/header.js
@@ -1,7 +1,7 @@
-// DOM(HTML 태그 로딩 완료시 실행)
-$(document).ready(function() {
+// // DOM(HTML 태그 로딩 완료시 실행)
+// $(document).ready(function() {
 
-});
+// });
 // 멀티미디어 리소스 로딩 완료 후 실행
 window.onload = function() {
   let navList = document.querySelectorAll(".gnb > li");
--- js/main_event.js
+++ js/main_event.js
@@ -1,9 +1,9 @@
-AOS.init();
+
 // DOM(HTML 태그 로딩 완료시 실행)
 $(document).ready(function() {
 
 });
 // 멀티미디어 리소스 로딩 완료 후 실행
 window.onload = function() {
-
+  AOS.init();
 };
