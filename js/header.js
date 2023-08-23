b9243db97888e3041bf6205b6da79202d74cf050
HamB20001013 <khb1535@gmail.com>
2023-08-23 11:52:23 +0900

    feat : header pc버전 완성
    header 반응형 작업 중

--- js/header.js
+++ js/header.js
@@ -4,5 +4,16 @@ $(document).ready(function() {
 });
 // 멀티미디어 리소스 로딩 완료 후 실행
 window.onload = function() {
-
+  let navList = document.querySelectorAll(".gnb > li");
+    
+  navList.forEach(function(navItem){
+    navItem.addEventListener("mouseover", function(){
+      this.querySelector(".submenu").style.height = "250px";
+    });
+  });
+  navList.forEach(function(navItem){
+    navItem.addEventListener("mouseout", function(){
+      this.querySelector(".submenu").style.height = "0px";
+    });
+  });
 };
--- js/main.js
+++ js/main.js
@@ -5,5 +5,5 @@ $(document).ready(function() {
 });
 // 멀티미디어 리소스 로딩 완료 후 실행
 window.onload = function() {
-
+  
 };
