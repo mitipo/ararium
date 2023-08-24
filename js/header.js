5887c7c32c58b91d94587fe92e079fa25e041263
HamB20001013 <133739490+HamB20001013@users.noreply.github.com>
2023-08-25 01:41:35 +0900

    feat : header 모바일 버전 제작 중
    PC, 태블릿 버전 완성

--- js/header.js
+++ js/header.js
@@ -1,22 +1,15 @@
-// // DOM(HTML 태그 로딩 완료시 실행)
-// $(document).ready(function() {
+// DOM(HTML 태그 로딩 완료시 실행)
+$(document).ready(function() {
+  $(".gnb > li").mouseover(function(){        
+    $(this).find(".submenu").stop().slideDown();  
+});
 
-// });
+$(".gnb > li").mouseout(function(){
+    $(this).find(".submenu").stop().slideUp();
+});
+});
 // 멀티미디어 리소스 로딩 완료 후 실행
 window.addEventListener("load", function() {
-  let navList = document.querySelectorAll(".gnb > li");
-    
-  navList.forEach(function(navItem){
-    navItem.addEventListener("mouseover", function(){
-      this.querySelector(".submenu").style.height = "250px";
-    });
-  });
-  navList.forEach(function(navItem){
-    navItem.addEventListener("mouseout", function(){
-      this.querySelector(".submenu").style.height = "0px";
-    });
-  });
-
   // 햄버거바 클릭하면 메뉴가 아래로 펼쳐짐
   const toggleBar = document.querySelector(".navbar_toggleBtn");
   let gnb = document.querySelector(".gnb");
