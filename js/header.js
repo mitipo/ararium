bb55e01e5e169f1e6de182bacfb7c968d887de4f
HamB20001013 <khb1535@gmail.com>
2023-08-25 17:01:33 +0900

    feat : footer 반응형 완성
    footer 반응형 완성

--- js/header.js
+++ js/header.js
@@ -2,14 +2,18 @@
 $(document).ready(function() {
   $(".gnb > li").mouseover(function(){        
     $(this).find(".submenu").stop().slideDown();  
-});
+  });
 
-$(".gnb > li").mouseout(function(){
+  $(".gnb > li").mouseout(function(){
     $(this).find(".submenu").stop().slideUp();
-});
+  });
 });
 // 멀티미디어 리소스 로딩 완료 후 실행
 window.addEventListener("load", function() {
+  // 토글메뉴 a태그 이벤트 막기
+  $('.navbar_toggleBtn').on("click",function(e){
+    e.preventDefault();
+  });
   // 햄버거바 클릭하면 메뉴가 아래로 펼쳐짐
   const hambergerBar = document.querySelector(".fa-bars");
   const xmark = document.querySelector(".fa-xmark");
