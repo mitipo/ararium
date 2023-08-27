bf5894e7fffa0b9a5fa76346b9546a0eba350ce0
HamB20001013 <133739490+HamB20001013@users.noreply.github.com>
2023-08-28 00:49:15 +0900

    feat : event swiper 작업 중
    swiper 작업 중

--- js/main_event.js
+++ js/main_event.js
@@ -1,9 +1,48 @@
-
 // DOM(HTML 태그 로딩 완료시 실행)
 $(document).ready(function() {
 
 });
 // 멀티미디어 리소스 로딩 완료 후 실행
 window.addEventListener("load", function() {
-  AOS.init();
+  // AOS.init();
+  new Swiper('.swiper', {    
+    autoplay: {
+      delay: 5000,
+    },
+    loop: true,
+    slidesPerView: 5,
+    spaceBetween: 30,
+    autoHeight: true,
+    pagination: {
+      el: '.promotion .swiper-pagination',
+      clickable: true,
+    },
+    
+    navigation: {
+      prevEl: '.event .swiper-button-prev',
+      nextEl: '.event .swiper-button-next',
+    },
+    breakpoints: {
+      480: {
+        slidesPerView: 2,
+        spaceBetween: 10,
+      },
+      760: {
+        slidesPerView: 2,
+        spaceBetween: 20,
+      },
+      1024: {
+        slidesPerView: 3,
+        spaceBetween: 30,
+      },
+      1280: {
+        slidesPerView: 4,
+        spaceBetween: 40,
+      },
+      1680: {
+        slidesPerView: 5,
+        spaceBetween: 50,
+      },
+    },
+  });
 });
