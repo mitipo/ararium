24befc327c4c7e9e6ce4446b36b1db6a3f151a4d
hyeongyu36 <hyeongyu36@gmail.com>
2023-08-28 18:14:27 +0900

    feat : 동물소개 내용추가, jquery 추가 및 수정
    동물소개 html 추가
    점 열고 닫기 jquery 추가 및 수정

--- js/main_marine_animals.js
+++ js/main_marine_animals.js
@@ -1,8 +1,34 @@
-
 // DOM(HTML 태그 로딩 완료시 실행)
-$(document).ready(function() {
+$(document).ready(function () {
+  // dot 클릭시 동물소개, 이미지 생성
+  $(".dot_1").on("click", function () {
+    $(".ani1").show(700);
+    // $(".marineanimal-ex1").show(1000);
+    $(".ani1").animate({ marginLeft: 13 + "%" }, 600);
+    setTimeout(function () {
+      $(".marineanimal-ex.01").stop().fadeIn();
+    }, 1000);
+  });
+  $(".dot_2").on("click", function () {
+    $(".ani2").show(700);
+    // $(".marineanimal-ex1").show(1000);
+    $(".ani2").animate({ marginLeft: 20 + "%" }, 600);
+    setTimeout(function () {
+      $(".marineanimal-ex.02").stop().fadeIn();
+    }, 1000);
+  });
+
+  // 닫기 클릭시 동물소개 숨기기
+  $("#marineanimal-x").on("click", function () {
+    $(".ani1").hide(500);
+    $(".marineanimal-ex.01").hide(500);
+  });
+  $("#marineanimal-x").on("click", function () {
+    $(".ani2").hide(500);
+    $(".marineanimal-ex.02").hide(500);
+  });
 });
 // 멀티미디어 리소스 로딩 완료 후 실행
-window.addEventListener("load", function() {
+window.addEventListener("load", function () {
   AOS.init();
 });
