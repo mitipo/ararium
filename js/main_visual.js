72ca2a5331f1bc1acdd28993fcfce2cc70f4271e
HamB20001013 <khb1535@gmail.com>
2023-08-30 14:51:05 +0900

    feat : 메인페이지 비주얼 섹션 작업 중
    메인페이지 비주얼 섹션 작업 중

--- js/main_visual.js
+++ js/main_visual.js
@@ -1,9 +1,26 @@
-
 // DOM(HTML 태그 로딩 완료시 실행)
 $(document).ready(function() {
 
 });
 // 멀티미디어 리소스 로딩 완료 후 실행
 window.addEventListener("load", function() {
-  AOS.init();
+  // AOS.init();
+  new Swiper('.rightcontent .swiper', {    
+    autoplay: {
+      delay: 5000,
+    },
+    loop: true,
+    slidesPerView: 1,
+    // spaceBetween: 10,
+    autoHeight: true,
+    pagination: {
+      el: '.promotion .swiper-pagination',
+      clickable: true,
+    },
+    
+    navigation: {
+      prevEl: '.rightcontent .fa-chevron-left',
+      nextEl: '.rightcontent .fa-chevron-right',
+    },
+  });
 });
